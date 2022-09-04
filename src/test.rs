4347827f5f40cfafe2ac253157e367cec7685df2
Nicolas BAUW <nbauw@hotmail.com>
2022-09-04 16:57:21 +0200

    More unit tests

--- src/test.rs
+++ src/test.rs
@@ -848,6 +848,36 @@ fn jp_jr_asm() {
 }
 
 #[test]
+fn ldi_asm() {
+    let mut c = CPU::new();
+    c.bus.write_byte(0x1000, 0x01);
+    c.bus.write_byte(0x1001, 0x02);
+    c.bus.write_byte(0x1002, 0x03);
+    c.bus.load_bin("bin/ldi.bin", 0).unwrap();
+    for _ in 0..3 {
+        c.execute();
+    }
+    assert_eq!(c.execute(), 16);
+    assert_eq!(0x1001, c.registers.get_hl());
+    assert_eq!(0x2001, c.registers.get_de());
+    assert_eq!(0x0002, c.registers.get_bc());
+    assert_eq!(0x01, c.bus.read_byte(0x2000));
+    assert_eq!(c.registers.flags.to_byte(), PF);
+    assert_eq!(c.execute(), 16);
+    assert_eq!(0x1002, c.registers.get_hl());
+    assert_eq!(0x2002, c.registers.get_de());
+    assert_eq!(0x0001, c.registers.get_bc());
+    assert_eq!(0x02, c.bus.read_byte(0x2001));
+    assert_eq!(c.registers.flags.to_byte(), PF);
+    assert_eq!(c.execute(), 16);
+    assert_eq!(0x1003, c.registers.get_hl());
+    assert_eq!(0x2003, c.registers.get_de());
+    assert_eq!(0x0000, c.registers.get_bc());
+    assert_eq!(0x03, c.bus.read_byte(0x2002));
+    assert_eq!(c.registers.flags.to_byte(), 0);
+}
+
+#[test]
 fn ld_b() {
     let mut c = CPU::new();
     c.registers.b = 0x11;
---
+++ tests/ldi.asm
@@ -0,0 +1,9 @@
+.target "z80"
+.format "bin"
+
+    LD  HL,0x1000
+    LD  DE,0x2000
+    LD  BC,0x0003
+    LDI 
+    LDI 
+    LDI
