5cd46a1f872543d387b4a7f8a3720f82ca18fe5b
Nicolas BAUW <nbauw@hotmail.com>
2023-08-19 18:00:31 +0200

    Doc update

--- src/lib.rs
+++ src/lib.rs
@@ -8,12 +8,12 @@
 //! c.reg.pc = 0x0100;                  // sets pc to 0x0100
 //! // Here we create a small machine code program for demo purpose.
 //! // Usually you will rather load an assembled code in memory with the load_bin function.
-//! c.bus.write_byte(0x0100, 0x3e);     // LD A,0x0F
-//! c.bus.write_byte(0x0101, 0x0F);
-//! c.bus.write_byte(0x0102, 0x3d);     // DEC A
-//! c.bus.write_byte(0x0103, 0xc2);     // JP NZ,0x0102
-//! c.bus.write_word(0x0104, 0x0102);
-//! c.bus.write_byte(0x0106, 0xc9);     // RET
+//! c.bus.borrow_mut().write_byte(0x0100, 0x3e);     // LD A,0x0F
+//! c.bus.borrow_mut().write_byte(0x0101, 0x0F);
+//! c.bus.borrow_mut().write_byte(0x0102, 0x3d);     // DEC A
+//! c.bus.borrow_mut().write_byte(0x0103, 0xc2);     // JP NZ,0x0102
+//! c.bus.borrow_mut().write_word(0x0104, 0x0102);
+//! c.bus.borrow_mut().write_byte(0x0106, 0xc9);     // RET
 //! loop {
 //!     c.execute();
 //!     if c.reg.pc == 0x0000 { break }
